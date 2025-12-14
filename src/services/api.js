a1369d1393c338230220d1bce6fa2719e6eac727
rodrigoromano23 <rr699785@gmail.com>
2025-12-14 19:53:44 -0300

    conect

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || "http://localhost:4000",
+  baseURL: import.meta.env.VITE_API_BASE || "https://nombre-backend.onrender.com",
   headers: {
     "Content-Type": "application/json"
   }
